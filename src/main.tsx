8bbbfdef7e2eec671fc6ff26834c93d2b1dc2c77
crevo2025 <takoyakicrevo@gmail.com>
2026-04-14 11:58:15 +0900

    テスト

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+// import {StrictMode} from 'react';
+// import {createRoot} from 'react-dom/client';
+// import App from './App.tsx';
+// import './index.css';
 
+console.log('main.tsx is executing');
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  rootElement.innerHTML = '<div style="color: blue; font-size: 24px; text-align: center;">React Mounting...</div>';
+  rootElement.innerHTML = '<div style="color: blue; font-size: 24px; text-align: center;">main.tsx is WORKING! (Blue)</div>';
 }
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-);
+// createRoot(document.getElementById('root')!).render(
+//     <App />
+// );
