cace615db3ae9a0937ac7ca2907572fa68621213
crevo2025 <takoyakicrevo@gmail.com>
2026-04-14 11:48:10 +0900

    テスト

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  rootElement.innerHTML = '<div style="color: blue; font-size: 24px; text-align: center;">React Mounting...</div>';
+}
+
 createRoot(document.getElementById('root')!).render(
     <App />
 );
